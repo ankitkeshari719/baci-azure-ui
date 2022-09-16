29b930b82f69641001e331364291e3a2eb2ab403
rajeshgulrajani-evoltech <112374911+rajeshgulrajani-evoltech@users.noreply.github.com>
2022-09-16 14:58:24 +1000

    Add files via upload

---
+++ FeatureFlags.ts
@@ -0,0 +1,47 @@
+import log from "loglevel";
+
+const env = process.env.REACT_APP_NODE_ENV?.toLocaleLowerCase();
+
+let featureFlags = {
+    feedback: true,
+    pulseCheck: true,
+    rateApp: false,
+    report: true,
+    stressTest: true,
+};
+
+let api_url = "";
+if (env === 'production') {
+    featureFlags = {
+        feedback: false,
+        pulseCheck: false,
+        rateApp: false,
+        report: true,
+        stressTest: false,
+    };
+    api_url = "https://baciapi.azurewebsites.net"
+    log.setLevel(log.levels.ERROR);
+} else if (env === 'test') {
+    featureFlags = {
+        feedback: true,
+        pulseCheck: true,
+        rateApp: false,
+        report: true,
+        stressTest: false,
+    };
+    api_url = "http://localhost:5010"
+    log.setLevel(log.levels.TRACE);
+} else if (env === 'development') {
+    featureFlags = {
+        feedback: true,
+        pulseCheck: true,
+        rateApp: false,
+        report: true,
+        stressTest: true,
+    };
+    api_url = "http://localhost:5010"
+    log.setLevel(log.levels.TRACE);
+}
+
+export const FEATURE_FLAGS_SET = featureFlags;
+export const API_URL = api_url
---
+++ SocketProvider.tsx
@@ -0,0 +1,18 @@
+import { useEffect,useState,createContext,ReactChild } from "react";
+import socketIOClient from "socket.io-client";
+import { API_URL } from '../constants/FeatureFlags';
+
+
+const ws =  socketIOClient(API_URL);
+//const ws = socketIOClient("https://baciapi.azurewebsites.net");
+
+
+export const SocketContext = createContext(ws)
+
+interface ISocketProvider {
+    children: ReactChild;
+}
+
+export const SocketProvider = (props:ISocketProvider) => (
+    <SocketContext.Provider value={ws}>{props.children}</SocketContext.Provider>
+);
