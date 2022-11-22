30dad3efc19c5751a3c5e41efcdab903af3fa2dd
jaikishanevoltech <117148253+jaikishanevoltech@users.noreply.github.com>
2022-11-22 13:20:44 +0530

    Update FeatureFlags.ts

--- src/constants/FeatureFlags.ts
+++ src/constants/FeatureFlags.ts
@@ -11,38 +11,38 @@ let featureFlags = {
     stressTest: true,
 };
 //let api_url = "http://localhost:8081";
-//let api_url = env_api_url;
-if (env == 'production') {
-    let featureFlags = {
-        feedback: false,
-        pulseCheck: false,
-        rateApp: false,
-        report: true,
-        stressTest: false,
-    };
-    let api_url = "https://prod-api-container-app.happywave-6f9d3247.australiaeast.azurecontainerapps.io";
-    log.setLevel(log.levels.ERROR);
-} else if (env == 'test') {
-    let featureFlags = {
-        feedback: true,
-        pulseCheck: true,
-        rateApp: false,
-        report: true,
-        stressTest: false,
-    };
-    let api_url = "https://baciapi.azurewebsites.net";
-    log.setLevel(log.levels.TRACE);
-} else if (env == 'development') {
-    let featureFlags = {
-        feedback: true,
-        pulseCheck: true,
-        rateApp: false,
-        report: true,
-        stressTest: true,
-    };
-    let api_url = "https://baciapi.azurewebsites.net";
-    log.setLevel(log.levels.TRACE);
-}
+let api_url = process.env.REACT_APP_API_URL_ENV;
+// if (env == 'production') {
+//     let featureFlags = {
+//         feedback: false,
+//         pulseCheck: false,
+//         rateApp: false,
+//         report: true,
+//         stressTest: false,
+//     };
+//     let api_url = "https://prod-api-container-app.happywave-6f9d3247.australiaeast.azurecontainerapps.io";
+//     log.setLevel(log.levels.ERROR);
+// } else if (env == 'test') {
+//     let featureFlags = {
+//         feedback: true,
+//         pulseCheck: true,
+//         rateApp: false,
+//         report: true,
+//         stressTest: false,
+//     };
+//     let api_url = "https://baciapi.azurewebsites.net";
+//     log.setLevel(log.levels.TRACE);
+// } else if (env == 'development') {
+//     let featureFlags = {
+//         feedback: true,
+//         pulseCheck: true,
+//         rateApp: false,
+//         report: true,
+//         stressTest: true,
+//     };
+//     let api_url = "https://baciapi.azurewebsites.net";
+//     log.setLevel(log.levels.TRACE);
+// }
 
 export const FEATURE_FLAGS_SET = featureFlags;
 export const API_URL = api_url;
