b86a7b85d4fc1e3d45b833ef1559617f4cba788e
mayuriningdalli1 <mayuri.ningdalli@evoltech.com.au>
2022-12-05 10:43:01 +0530

    'chnages_331'

--- src/elements/SubToolbar.tsx
+++ src/elements/SubToolbar.tsx
@@ -5,6 +5,7 @@ import {
   ListItemIcon,
   Popover,
   TextField,
+  Tooltip,
   Typography,
 } from '@mui/material';
 import React, { useEffect } from 'react';
@@ -307,8 +308,11 @@ const SubToolbar = (props: any) => {
                   marginRight: '8px',
                 }}
               />
+              <Tooltip title={user.userNickname}>
               <ListItemText sx={{'&& .MuiListItemText-primary':{minWidth: '100px', overflow: 'hidden', textOverflow : 'ellipsis'}
                 }} primary={user.userNickname} />
+              </Tooltip>
+            
             </MenuItem>
           ))}
         </Select>
