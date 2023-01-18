886b7f52560078095c723dac49450b382fa75907
Ankit Keshari <GS-1544@GSLAB.COM>
2023-01-18 23:15:54 +0530

    CSS issue 6

--- src/components/OutlinedButton.tsx
+++ src/components/OutlinedButton.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import './styles.scss';
 
 type Props = {
@@ -16,7 +16,9 @@ export function OutlinedButton({ name, onClick, style }: Props) {
       onClick={onClick}
       sx={{ ...style }}
     >
-      {name}
+      <Typography className="customizeButtonText" component="span">
+        {name}
+      </Typography>
     </Button>
   );
 }
