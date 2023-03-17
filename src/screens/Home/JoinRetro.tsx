98db55311dbbe47c273fea4341cfcf41ec32430f
Ankit Keshari <GS-1544@GSLAB.COM>
2023-03-17 13:11:09 +0530

    Dialog issue on join rectro page

--- src/screens/Home/JoinRetro.tsx
+++ src/screens/Home/JoinRetro.tsx
@@ -306,7 +306,7 @@ export function JoinRetro() {
   return (
     <>
       {isXsUp ? (
-        <Box sx={{ overflowY: 'auto' }}>
+        <Box sx={{  height: 'calc(100vh)', overflowY: 'auto' }}>
           {/* <DeploymentPopUp /> */}
           <LandingLayout></LandingLayout>
           <Box
@@ -315,7 +315,7 @@ export function JoinRetro() {
               alignItems: 'center',
               justifyContent: 'center',
               flexDirection: 'column',
-              height: 'calc(50vh)',
+              // height: 'calc(50vh)',
               overflowY: 'auto',
             }}
           >
