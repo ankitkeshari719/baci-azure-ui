b8779438112a623fdcd5e2c96ff3cdec9625531e
Ankit Keshari <GS-1544@GSLAB.COM>
2023-05-09 11:12:30 +0530

    Hide actions

--- src/screens/ManageActions/ActionsList.tsx
+++ src/screens/ManageActions/ActionsList.tsx
@@ -4,6 +4,9 @@ import './styles.scss';
 import { ActionInterface } from '../../types';
 import ActionItem from './ActionItem';
 import { Box, List } from '@mui/material';
+import { BoardContext } from '../../contexts/BoardContext';
+import { GlobalContext } from '../../contexts/GlobalContext';
+import ZeroActions from './ZeroActions';
 
 type Props = {
   allActions: ActionInterface[];
@@ -11,20 +14,41 @@ type Props = {
 };
 
 export default function ActionsList({ allActions, handleToggleAction }: Props) {
-  console.log('allActions:::',allActions);
+  const {
+    state: { actionsData },
+    commitAction,
+  } = React.useContext(BoardContext);
+  const [global] = React.useContext(GlobalContext);
+  const [actionCountForCurrentUser, setActionCountForCurrentUser] =
+    React.useState<number>(0);
+
+  React.useEffect(() => {
+    let actionCount = 0;
+    actionsData.actions.map(action => {
+      if (global.user.id === action.createdBy) {
+        actionCount++;
+      }
+    });
+    setActionCountForCurrentUser(actionCount);
+  }, [actionsData]);
+
   return (
     <Box sx={{ width: '100%', height: 'var(--app-height)', overflowY: 'auto' }}>
-      <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-        {allActions.map(action => {
-          return (
-            <ActionItem
-              action={action}
-              key={action.id}
-              handleToggleAction={handleToggleAction}
-            />
-          );
-        })}
-      </List>
+      {actionCountForCurrentUser === 0 ? (
+        <ZeroActions />
+      ) : (
+        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+          {allActions.map(action => {
+            return (
+              <ActionItem
+                action={action}
+                key={action.id}
+                handleToggleAction={handleToggleAction}
+              />
+            );
+          })}
+        </List>
+      )}
     </Box>
   );
 }
