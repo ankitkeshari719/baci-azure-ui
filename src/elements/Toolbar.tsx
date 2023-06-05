7c774191716412c593b522cf0510c8bef2bc913a
Ankit Keshari <GS-1544@GSLAB.COM>
2023-06-05 13:36:42 +0530

    Worked on the finish retro page

--- src/elements/Toolbar.tsx
+++ src/elements/Toolbar.tsx
@@ -731,95 +731,120 @@ const Toolbar = (props: any) => {
               marginTop: '24px',
             }}
           />
-          <Box sx={{ marginTop: '24px' }}>
-            <Box className="d-flex justify-content-center align-items-center">
-              <Typography className="textTypeFour">
-                {manageActions.length}
-                {manageActions.length === 1 ? ' Action' : ' Actions'} Identified
+          {manageActions.length === 0 ? (
+            <Box
+              sx={{ marginTop: '24px' }}
+              className="d-flex justify-content-center align-items-center"
+            >
+              <Typography
+                sx={{
+                  fontFamily: 'Poppins',
+                  fontStyle: 'normal',
+                  fontWeight: 600,
+                  fontSize: '20px',
+                  lineHeight: '24px',
+                  textAlign: 'center',
+                  letterSpacing: '0.3px',
+                  color: '#EE7538',
+                }}
+              >
+                No Actions Identified!
               </Typography>
             </Box>
-            <Box>
-              {manageActions.map((action: ActionInterface, index: number) => {
-                const labelId = `action-label-${action.id}`;
-                return (
-                  <Row
-                    style={{ marginTop: index > 0 ? '16px' : '0px' }}
-                    key={labelId}
-                  >
-                    <Col
-                      xs="12"
-                      className="d-flex justify-content-start align-items-center"
+          ) : (
+            <Box sx={{ marginTop: '24px' }}>
+              <Box className="d-flex justify-content-center align-items-center">
+                <Typography className="textTypeFour">
+                  {manageActions.length}
+                  {manageActions.length === 1 ? ' Action' : ' Actions'}{' '}
+                  Identified
+                </Typography>
+              </Box>
+              <Box>
+                {manageActions.map((action: ActionInterface, index: number) => {
+                  const labelId = `action-label-${action.id}`;
+                  return (
+                    <Row
+                      style={{ marginTop: index > 0 ? '16px' : '0px' }}
+                      key={labelId}
                     >
-                      <svg
-                        width="20"
-                        height="20"
-                        viewBox="0 0 20 20"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
+                      <Col
+                        xs="12"
+                        className="d-flex justify-content-start align-items-center"
                       >
-                        <path
-                          d="M9.48073 1.4987C9.67288 1.03673 10.3273 1.03673 10.5195 1.4987L12.6454 6.61016C12.7264 6.80492 12.9096 6.93799 13.1199 6.95484L18.6381 7.39724C19.1369 7.43722 19.3391 8.05964 18.9591 8.38514L14.7548 11.9866C14.5946 12.1238 14.5246 12.3391 14.5736 12.5443L15.858 17.9292C15.9741 18.4159 15.4447 18.8005 15.0177 18.5397L10.2933 15.6541C10.1133 15.5441 9.8869 15.5441 9.7069 15.6541L4.98251 18.5397C4.55551 18.8005 4.02606 18.4159 4.14215 17.9292L5.42664 12.5443C5.47558 12.3391 5.40562 12.1238 5.24543 11.9866L1.04111 8.38514C0.661119 8.05964 0.863352 7.43722 1.36209 7.39724L6.88034 6.95484C7.0906 6.93799 7.27375 6.80492 7.35476 6.61016L9.48073 1.4987Z"
-                          fill="#FBBC05"
-                          stroke="#FBBC05"
-                          strokeWidth="1.5"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
-                      <Typography
-                        color="black"
-                        style={{
-                          fontWeight: 400,
-                          fontSize: '14px',
-                          color: '#FBBC05',
-                          marginLeft: '6px',
-                        }}
-                      >
-                        {action.reacts.length === 0 ? 0 : action.reacts.length}
-                      </Typography>
-                      <Box
-                        sx={{
-                          marginLeft:
-                            action?.assigneeAvatar === '' ||
-                            action.assigneeAvatar === undefined
-                              ? '12px'
-                              : '24px',
-                        }}
-                      >
-                        {action?.assigneeAvatar === '' ||
-                        action.assigneeAvatar === undefined ? (
-                          <Icons.UserCircle
-                            style={{
-                              color: '#CCCCCC',
-                              width: '60px',
-                              height: '50px',
-                            }}
+                        <svg
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M9.48073 1.4987C9.67288 1.03673 10.3273 1.03673 10.5195 1.4987L12.6454 6.61016C12.7264 6.80492 12.9096 6.93799 13.1199 6.95484L18.6381 7.39724C19.1369 7.43722 19.3391 8.05964 18.9591 8.38514L14.7548 11.9866C14.5946 12.1238 14.5246 12.3391 14.5736 12.5443L15.858 17.9292C15.9741 18.4159 15.4447 18.8005 15.0177 18.5397L10.2933 15.6541C10.1133 15.5441 9.8869 15.5441 9.7069 15.6541L4.98251 18.5397C4.55551 18.8005 4.02606 18.4159 4.14215 17.9292L5.42664 12.5443C5.47558 12.3391 5.40562 12.1238 5.24543 11.9866L1.04111 8.38514C0.661119 8.05964 0.863352 7.43722 1.36209 7.39724L6.88034 6.95484C7.0906 6.93799 7.27375 6.80492 7.35476 6.61016L9.48073 1.4987Z"
+                            fill="#FBBC05"
+                            stroke="#FBBC05"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                           />
-                        ) : (
-                          <Avatar
-                            avatar={action?.assigneeAvatar}
-                            onClickAvatar={() => {}}
-                            css={{
-                              width: '40px',
-                              height: '40px',
-                              borderRadius: '50%',
-                              border: 'none',
-                            }}
-                          ></Avatar>
-                        )}
-                      </Box>
-                      <Typography
-                        style={{ marginLeft: '12px' }}
-                        className="actionValue"
-                      >
-                        {action.value}
-                      </Typography>
-                    </Col>
-                  </Row>
-                );
-              })}
+                        </svg>
+                        <Typography
+                          color="black"
+                          style={{
+                            fontWeight: 400,
+                            fontSize: '14px',
+                            color: '#FBBC05',
+                            marginLeft: '6px',
+                          }}
+                        >
+                          {action.reacts.length === 0
+                            ? 0
+                            : action.reacts.length}
+                        </Typography>
+                        <Box
+                          sx={{
+                            marginLeft:
+                              action?.assigneeAvatar === '' ||
+                              action.assigneeAvatar === undefined
+                                ? '12px'
+                                : '24px',
+                          }}
+                        >
+                          {action?.assigneeAvatar === '' ||
+                          action.assigneeAvatar === undefined ? (
+                            <Icons.UserCircle
+                              style={{
+                                color: '#CCCCCC',
+                                width: '60px',
+                                height: '50px',
+                              }}
+                            />
+                          ) : (
+                            <Avatar
+                              avatar={action?.assigneeAvatar}
+                              onClickAvatar={() => {}}
+                              css={{
+                                width: '40px',
+                                height: '40px',
+                                borderRadius: '50%',
+                                border: 'none',
+                              }}
+                            ></Avatar>
+                          )}
+                        </Box>
+                        <Typography
+                          style={{ marginLeft: '12px' }}
+                          className="actionValue"
+                        >
+                          {action.value}
+                        </Typography>
+                      </Col>
+                    </Row>
+                  );
+                })}
+              </Box>
             </Box>
-          </Box>
+          )}
           <Divider
             color="#EE7538"
             style={{
