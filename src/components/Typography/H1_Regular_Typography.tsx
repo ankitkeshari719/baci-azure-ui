45dae2549755422e3ad7ee491f41aec62a5a1c3a
Ankit Keshari <GS-1544@GSLAB.COM>
2023-06-08 11:11:42 +0530

    Worked on the PB-730 issue

---
+++ src/components/Typography/H1_Regular_Typography.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Typography } from '@mui/material';
+import './styles.scss';
+
+type Props = {
+  name: string;
+  onClick: (...param: any) => void;
+  style?: any;
+};
+
+export function H1RegularTypography({ name, onClick, style }: Props) {
+  return (
+    <Typography className="saveButtonText" component="span">
+      {name}
+    </Typography>
+  );
+}
