e5ed26cf05c13473497ee3dbc5f9646ed0e0f202
Ankit Keshari <GS-1544@GSLAB.COM>
2023-06-30 11:17:23 +0530

    Added analytics routing

--- src/components/Elements/leftBar/LeftBar.tsx
+++ src/components/Elements/leftBar/LeftBar.tsx
@@ -1,120 +1,151 @@
-import { Box, Tooltip } from "@mui/material";
+import { Box, Tooltip } from '@mui/material';
 
 import './LeftBar.scss';
 
 import {
-
-    HomeIcon, ViewColumnsIcon, ClipboardDocumentCheckIcon, ChartBarIcon, Square3Stack3DIcon, UserGroupIcon, Cog8ToothIcon, QuestionMarkCircleIcon,
-
-    BellIcon,
-
+  HomeIcon,
+  ViewColumnsIcon,
+  ClipboardDocumentCheckIcon,
+  ChartBarIcon,
+  Square3Stack3DIcon,
+  UserGroupIcon,
+  Cog8ToothIcon,
+  QuestionMarkCircleIcon,
+  BellIcon,
 } from '@heroicons/react/24/outline';
 
-import React from "react";
+import React from 'react';
 
-import Avatar from "../Avatar";
+import Avatar from '../Avatar';
 
-import { GlobalContext } from "../../../contexts/GlobalContext";
+import { GlobalContext } from '../../../contexts/GlobalContext';
 
-import { LazyLoadImage } from "react-lazy-load-image-component";
-
-
-
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { useNavigate } from 'react-router-dom';
 
 const LeftBar = () => {
+  const navigate = useNavigate();
+  const [selectedMenu, setSelectedMenu] = React.useState('');
 
-    const [selectedMenu, setSelectedMenu] = React.useState("");
+  const [{ user }] = React.useContext(GlobalContext);
 
-    const [{  user }] =
+  const menuArray = [
+    { id: 1, label: 'Home', icon: HomeIcon, routeTo: '', disabled: false },
 
-    React.useContext(GlobalContext);
+    {
+      id: 2,
+      label: 'Retros',
+      icon: ViewColumnsIcon,
+      routeTo: '',
+      disabled: false,
+    },
 
-    const menuArray = [
+    {
+      id: 3,
+      label: 'Workspace',
+      icon: ClipboardDocumentCheckIcon,
+      routeTo: '',
+      disabled: false,
+    },
 
-        { id: 1, label: 'Home', icon: HomeIcon, routeTo: '', disabled: false },
+    {
+      id: 4,
+      label: 'Analytics',
+      icon: ChartBarIcon,
+      routeTo: '/analytics/',
+      disabled: false,
+    },
 
-        { id: 2, label: 'Retros', icon: ViewColumnsIcon, routeTo: '', disabled: false },
+    {
+      id: 5,
+      label: 'Templates',
+      icon: Square3Stack3DIcon,
+      routeTo: '',
+      disabled: false,
+    },
 
-        { id: 3, label: 'Workspace', icon: ClipboardDocumentCheckIcon, routeTo: '', disabled: false },
+    {
+      id: 6,
+      label: 'Users',
+      icon: UserGroupIcon,
+      routeTo: '',
+      disabled: false,
+    },
 
-        { id: 4, label: 'Analytics', icon: ChartBarIcon, routeTo: '', disabled: false },
+    {
+      id: 7,
+      label: 'Settings',
+      icon: Cog8ToothIcon,
+      routeTo: '',
+      disabled: false,
+    },
+  ];
 
-        { id: 5, label: 'Templates', icon: Square3Stack3DIcon, routeTo: '', disabled: false },
+  const bottomMenuArray = [
+    {
+      id: 8,
+      label: 'Help',
+      icon: QuestionMarkCircleIcon,
+      routeTo: '',
+      disabled: false,
+    },
+    {
+      id: 9,
+      label: 'Notifications',
+      icon: BellIcon,
+      routeTo: '',
+      disabled: false,
+    },
+  ];
 
-        { id: 6, label: 'Users', icon: UserGroupIcon, routeTo: '', disabled: false },
-
-        { id: 7, label: 'Settings', icon: Cog8ToothIcon, routeTo: '', disabled: false },
-
-    ]
-
-    const bottomMenuArray = [
-
-        { id: 8, label: 'Help', icon: QuestionMarkCircleIcon, routeTo: '', disabled: false }, { id: 9, label: 'Notifications', icon: BellIcon, routeTo: '', disabled: false },
-
-
-
-
-    ]
-
-    return (<><Box className="leftBarContainer" >
-
+  return (
+    <>
+      <Box className="leftBarContainer">
         <Box className="topContainer">
+          <Box>
+            <img src="../images/r_MenuHeader.png" style={{ width: '56px' }} />
+          </Box>
 
-            <Box>
-
-                <img src="../images/r_MenuHeader.png" style={{ width: '56px' }} />
-
-            </Box>
-
-            {
-
-                menuArray.map((menu, index) => {
-
-                    return (<Tooltip title={menu.label} key={menu.label}>
-
-                        <menu.icon className={menu.label == selectedMenu ? "menuIconSelected" : "menuIcon"} onClick={() => { setSelectedMenu(menu.label) }} />
-
-                    </Tooltip>)
-
-                })
-
-            }
-
+          {menuArray.map((menu, index) => {
+            return (
+              <Tooltip title={menu.label} key={menu.label}>
+                <menu.icon
+                  className={
+                    menu.label == selectedMenu ? 'menuIconSelected' : 'menuIcon'
+                  }
+                  onClick={() => {
+                    setSelectedMenu(menu.label);
+                    navigate(menu.routeTo);
+                  }}
+                />
+              </Tooltip>
+            );
+          })}
         </Box>
 
-
-
-
         <Box className="bottomContainer">
+          {bottomMenuArray.map((menu, index) => {
+            return (
+              <Tooltip title={menu.label} key={menu.label}>
+                <menu.icon
+                  className={
+                    menu.label == selectedMenu ? 'menuIconSelected' : 'menuIcon'
+                  }
+                  onClick={() => {
+                    setSelectedMenu(menu.label);
+                  }}
+                />
+              </Tooltip>
+            );
+          })}
 
-            {
-
-                bottomMenuArray.map((menu, index) => {
-
-                    return (<Tooltip title={menu.label} key={menu.label}>
-
-                        <menu.icon className={menu.label == selectedMenu ? "menuIconSelected" : "menuIcon"} onClick={() => { setSelectedMenu(menu.label) }} />
-
-                    </Tooltip>)
-
-                })
-
-            }
-
-              <Tooltip title={user?.name + ''}>
-
+          <Tooltip title={user?.name + ''}>
             <span>
-
               {user?.avatar ? (
-
                 <Avatar
-
                   avatar={user?.avatar}
-
                   onClickAvatar={() => {}}
-
                   css={{
-
                     width: '48px',
 
                     height: '48px',
@@ -122,57 +153,33 @@ const LeftBar = () => {
                     borderRadius: '50%',
 
                     border: 'none',
-
                   }}
-
                 ></Avatar>
+              ) : (
+                <LazyLoadImage
+                  width="48px !important"
+                  height="48px !important"
+                  style={{
+                    borderRadius: '50%',
 
-              ):<LazyLoadImage
-
-                width="48px !important"
-
-                height="48px !important"
-
-                style={{
-
-                  borderRadius: '50%',
-
-                  border: 'none',
-
-                }}
-
-                src={'/svgs/DefaultUser.svg'}
-
-              ></LazyLoadImage>}
-
+                    border: 'none',
+                  }}
+                  src={'/svgs/DefaultUser.svg'}
+                ></LazyLoadImage>
+              )}
             </span>
-
           </Tooltip>
-
         </Box>
+      </Box>
 
-    </Box>
-
-        {/* <Box sx={{ display: 'flex', position: 'abosolute', left: '72px', zIndex: '1000', background: 'white' }}>
+      {/* <Box sx={{ display: 'flex', position: 'abosolute', left: '72px', zIndex: '1000', background: 'white' }}>
 
 
 
 
         </Box> */}
-
-
-
-
     </>
-
-
-
-
-    );
-
-}
-
-
-
+  );
+};
 
 export default LeftBar;
